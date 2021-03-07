16d21328d64d53e6dab3aaf7599f112f6b32eb8c
Paweł Bucki <mrpawrob@gmail.com>
2021-03-07 13:41:06 +0100

    Added [config.properties]

--- app/src/main/java/pl/java/zzpj/Main.java
+++ app/src/main/java/pl/java/zzpj/Main.java
@@ -1,12 +1,29 @@
 package pl.java.zzpj;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
 
     public static void main( String[] args )
     {
         Person p = new Person("Jan", "Karabińczyk");
         System.out.print(p);
-        p.setName("John");
+        p.setName(getPersonNameProperty());
         System.out.print(p);
+    }
+
+    public static String getPersonNameProperty() {
+        Properties properties = new Properties();
+        String propertiesFileName = "config.properties";
+
+        try(InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(propertiesFileName)) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + propertiesFileName);
+            e.printStackTrace();
+        }
+        return properties.getProperty("name");
     }
 }
